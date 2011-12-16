488988a9ce68ea37b12a7ee65b88d8f612b66ea0
zjhiphop <zjhiphop@gmail.com>
2011-12-16 14:54:17 +0800

    update commit cmd file

--- commit.cmd
+++ commit.cmd
@@ -1,7 +1,35 @@
 @echo off
 cls
-cd /d %cmd%
+@echo --------------------------
+@echo 1.get project
+@echo 2.push project
+@echo --------------------------
+:continue
+set /p t=make your chose:
+if %t%==1 (
+	git fetch
+	git diff > .diff 
+	echo difference from your brunch to server:
+	...........................................
+	type .diff
+	...........................................
+	pause & goto :eof
+)
+if %t%==2 (
+	call  :commit
+	git diff > .diff 
+	echo difference from your brunch to server:
+	...........................................
+	type .diff
+	...........................................
+	pause & goto :eof
+) else (
+ @echo your chose is invalid!
+ goto :continue
+)
 
+:commit
+cd /d %cmd%
 set /p c=please input comments:
 if "%c%"=="" (
   set /p c=you must input commit msg first:
@@ -10,6 +38,5 @@ if "%c%"=="" (
   echo commit failed!
   goto :eof
 )
-
 git add . &  git commit -m "%c%" & git push
 pause
