ca1fbf01bcf38dee7b0afe2b7d2c53c8bd733bb8
zjhiphop <zjhiphop@gmail.com>
2011-12-19 10:30:27 +0800

    add some basic code

--- Client/script/main.js
+++ Client/script/main.js
@@ -1,3 +1,24 @@
-require(['jquery','widget/epaper','../lib/jquery-jtemplates_uncompressed','../lib/jquery-ui'],function($,epaper){
-	$.epaper.init({epaper_wrapper:'#container',src: "imgs/eg_epaper.jpg"});
+require(['jquery','js/widget/epaper','js/helper/order!lib/jquery-jtemplates_uncompressed','lib/jquery-ui'],function($p){
+	$p.epaper.init({epaper_wrapper:'#container',src: "imgs/eg_epaper.jpg"});
 });
+define(function(require) {
+  var lamp=require("js/nls/lamp");
+  console.log(lamp.testMessage);
+});
+/*
+require(["some/module", "text!some/module.html!strip", "text!some/module.css"],
+    function(module, html, css) {
+        //the html variable will be the text
+        //of the some/module.html file
+        //the css variable will be the text
+        //of the some/module.css file.
+    }
+);
+*/
+require(["js/helper/text!http://cns-812:8111/et_refac/client/package.json?callback=define"],
+    function (data) {
+        //The dta object will be the API response for the
+        //JSONP data call.
+        console.log(data);
+    }
+);
---
+++ Client/script/nls/lamp.js
@@ -0,0 +1,5 @@
+define(["js/helper/i18n!js/nls/color"], function(color) {
+    return {
+        testMessage: "The name for red in this locale is: " + color.red
+    }
+});
